bd6098095e399577dcc6309dc7f584de5c7d2324
stiggu <abelmunda@hotmail.com>
2022-04-11 15:59:55 -0500

    App now uses TS directly instead of converting TS to JS.

--- type-script/src/index.ts
+++ type-script/src/index.ts
@@ -4,9 +4,9 @@ const app = express();
 const port = 8080;
 
 app.get('/', (req, res) => {
-    res.send('hey!');
+    res.send('AHA');
 });
 
 app.listen(port, () => {
-    console.log(`Listening on https://localhost:${port}`);
+    console.log(`Listening on https://127.0.0.1:${port}`);
 });
