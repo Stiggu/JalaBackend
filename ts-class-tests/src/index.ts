c214a51f9ddf072082de47f311b6e95ec3fd7ca1
stiggu <abelmunda@hotmail.com>
2022-04-12 10:33:00 -0500

    Added missing files on certain folders

---
+++ ts-class-tests/src/index.ts
@@ -0,0 +1,49 @@
+// import express from "express";
+
+// const app = express();
+// const port = 8080;
+
+// app.get('/', (req, res) => {
+//     res.send('AHA');
+// });
+
+// app.listen(port, () => {
+//     console.log(`Listening on https://127.0.0.1:${port}`);
+// });
+
+
+// Any / Unknown
+// const z: unknown = 30;
+// const y = z === 123;
+// if(typeof z === 'number'){
+//     const x = z +10;
+// }
+// const a: any = 30;
+
+// Bools
+// const a = true;
+//illegal
+// var b = false;
+// const c = true;
+// let d: boolean = true;
+// let e: true = true;
+
+// Number
+// let a = 132;
+// var b = Infinity * .1;
+// const c = 5678;
+// let d = a < c;
+// let d2 = a / 0;
+// const pi: 3.14 = 3.14;
+// const m = 1_000_000;
+// const mx2 = 2_000_000;
+
+// BigInt - Target 2020+
+// const a = 1234n;
+
+
+
+
+
+
+
