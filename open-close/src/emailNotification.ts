ff1756c40bf336eb1c0189e5b336dac806fa7a14
Abel Mundarain <abel.mundarain@jala-foundation.org>
2022-05-11 08:06:11 -0500

    Added another notification for tests

---
+++ open-close/src/emailNotification.ts
@@ -0,0 +1,8 @@
+﻿import {User} from "./user";
+import INotification from "./INotification";
+
+export default class EmailNotification implements INotification{
+    notification(user: User, message:string): void{
+        console.log(`[Email]: ${message}`);
+    }
+}
--- open-close/src/index.ts
+++ open-close/src/index.ts
@@ -2,8 +2,10 @@ import express from 'express';
 import { User } from './user';
 import { NotificationCenter } from './notification-center';
 import FacebookNotification from "./facebookNotification";
+import EmailNotification from "./emailNotification";
 
 let user = new User();
 
 const notCenter = new NotificationCenter();
 notCenter.notify(new FacebookNotification(), user, 'test');
+notCenter.notify(new EmailNotification(), user, 'test');
